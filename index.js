4ab1febc28be03c4d2998eb3e3e687056b0f3b83
firmankhoiril <firmankhoiril13@gmail.com>
2023-03-02 20:18:31 +0700

    hi

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-const router = express.Router();
 dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
